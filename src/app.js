7074bf57fc6c49b2cf56e83ca64b9c759168789f
yubin <shin.yubin18@gmail.com>
2023-05-03 22:35:36 +0900

    Init : 초기 세팅
    필요한 모듈 설치 및
    prettierrc, eslintrc 추가해두었습니다.

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+
+const DB_URL =
+  process.env.MONGODB_URL ||
+  "MongoDB 서버 주소가 설정되지 않았습니다.";
+
+mongoose.set("strictQuery", false);
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (error) =>
+  console.error("\nMongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
+);
+db.once("connected", () =>
+  console.log("정상적으로 MongoDB 서버에 연결되었습니다.  " + DB_URL)
+);
+
+// CORS 에러 방지
+app.use(cors());
+
+// Content-Type: application/json 형태의 데이터를 인식하고 핸들링할 수 있게 함.
+app.use(express.json());
+
+// Content-Type: application/x-www-form-urlencoded 형태의 데이터를 인식하고 핸들링할 수 있게 함.
+app.use(express.urlencoded({ extended: false }));
+
+// 라우팅
+app.get('/', async (req, res) => {
+  res.send('백엔드루트페이지')
+});
+
+module.exports = { app };
