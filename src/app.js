6ef786133daf05292a3ffa1dc2e0d4a6fbc3796e
Saem <saemilee01@gmail.com>
2023-05-08 20:31:38 +0900

    chores: 초기 세팅

--- src/app.js
+++ src/app.js
@@ -1,26 +1,25 @@
-const cors = require("cors");
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
 
 const DB_URL =
-  process.env.MONGODB_URL ||
-  "MongoDB 서버 주소가 설정되지 않았습니다.";
+	process.env.MONGODB_URL || 'MongoDB 서버 주소가 설정되지 않았습니다.';
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false);
 mongoose.connect(DB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 
-db.on("error", (error) =>
-  console.error("\nMongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
+db.on('error', error =>
+	console.error('\nMongoDB 연결에 실패하였습니다...\n' + DB_URL + '\n' + error),
 );
-db.once("connected", () =>
-  console.log("정상적으로 MongoDB 서버에 연결되었습니다.  " + DB_URL)
+db.once('connected', () =>
+	console.log('정상적으로 MongoDB 서버에 연결되었습니다.  ' + DB_URL),
 );
 
 // CORS 에러 방지
@@ -34,7 +33,9 @@ app.use(express.urlencoded({ extended: false }));
 
 // 라우팅
 app.get('/', async (req, res) => {
-  res.send('백엔드루트페이지')
+	res.send('백엔드루트페이지');
 });
-
+app.listen(process.env.PORT, () => {
+	console.log(`http://localhost:${process.env.PORT}`);
+});
 module.exports = { app };
