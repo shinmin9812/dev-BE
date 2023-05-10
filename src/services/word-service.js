12a2cdf4aa02360b2fb3486a452efc3fb2542222
yubin <shin.yubin18@gmail.com>
2023-05-10 16:57:40 +0900

    Add : 단어장 인증 추가

--- src/services/word-service.js
+++ src/services/word-service.js
@@ -7,6 +7,7 @@ const {
 } = require('../db/schemas/word-schema');
 
 class WordService {
+
 	async findOneById({ short_id: id }) {
 		const word = await wordDAO.findOneById({ short_id: id });
 		if (!word) {
@@ -28,12 +29,15 @@ class WordService {
 	}
 
 	async createOne(params) {
+
+		/** 없는 단어장을 기재하여 추가하려한다면 */
 		const isBookExist = await BookModel.find({ name: params.book });
 		if (!isBookExist) {
 			const err = new Error('새로운 단어를 추가하지 못했습니다.');
 			err.status = 422;
 			throw err;
 		}
+		/** 잘못된 요청을 받았다면 */
 		const word = await wordDAO.createOne(params);
 		if (!word) {
 			const err = new Error('새로운 단어를 추가하지 못했습니다.');
@@ -44,6 +48,16 @@ class WordService {
 	}
 
 	async createMany(params) {
+
+		/** 없는 단어장을 기재하여 추가하려한다면 */
+		const isBookExist = await BookModel.find({ name: params.book });
+		if (!isBookExist) {
+			const err = new Error('새로운 단어를 추가하지 못했습니다.');
+			err.status = 422;
+			throw err;
+		}
+
+		/** 잘못된 요청을 받았다면 */
 		const words = await wordDAO.createMany(params);
 		if (!words) {
 			const err = new Error('새로운 단어를 추가하지 못했습니다.');
@@ -54,6 +68,16 @@ class WordService {
 	}
 
 	async updateOne(find, update) {
+
+		/** 없는 단어장을 기재하여 수정하려한다면 */
+		const isBookExist = await BookModel.find({ name: params.book });
+		if (!isBookExist) {
+			const err = new Error('새로운 단어를 추가하지 못했습니다.');
+			err.status = 422;
+			throw err;
+		}
+
+		/** 잘못된 요청을 받았다면 */
 		const word = await wordDAO.updateOne(find, update);
 		if (!word) {
 			const err = new Error('단어를 수정하지 못했습니다.');
