1444787f762a49149315734ec790dfe1eb722905
yubin <shin.yubin18@gmail.com>
2023-05-12 10:34:10 +0900

    Fix : bookrouter 에러 처리

--- src/db/schemas/word-schema.js
+++ src/db/schemas/word-schema.js
@@ -7,7 +7,7 @@ const WordSchema = new Schema(
 		short_id: shortId,
 		word: { type: String, required: true },
 		meanings: { type: Array, required: true },
-		status: { type: Number, enum: [0, 1, 2], required: true, default: 0 },
+		status: { type: Number, enum: [0, 1, 2], required: false, default: 0 },
 		/**0:미분류, 1:암기완료, 2:헷갈림 */
 		book: { type: String, default: '기본단어장' },
 	},
