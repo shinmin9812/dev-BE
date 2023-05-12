00ad87d33e4cb93dec319dcb27741cb934527142
lookethis <113021028+lookethis@users.noreply.github.com>
2023-05-12 23:49:53 +0900

    book 3계층에 ownerEmail를 식별자로 하여 전면 수정, 토큰 추가

--- src/routers/book-router.js
+++ src/routers/book-router.js
@@ -5,60 +5,45 @@ const verifyToken = require('../middlewares/auth-handler');
 
 const bookRouter = Router();
 
-bookRouter.get('/', 
-	asyncHandler(async (req, res) => {
-		const result = await bookService.findAll();
-		res.status(200).json(result);
-	})
-);
+bookRouter.get('/', verifyToken, asyncHandler(async (req, res) => {
+	const result = await bookService.findAll();
+	res.status(200).json(result);
+}));
 
-bookRouter.get('/:id', 
-  asyncHandler(async (req, res) => {
-		const { id } = req.params;
-		const result = await bookService.findOneById({ short_id: id });
-		if (!result) {
-			return next(new Error('Book not found'));  
-		}
-		res.status(200).json(result);
-	})
-);
+bookRouter.get('/me', verifyToken, asyncHandler(async (req, res, next) => {
+	const result = await bookService.findAllByUser({ ownerEmail: req.user.email });
+	if (!result) {
+		return next(new Error('No books found for this user'));  
+	}
+	res.status(200).json(result);
+}));
 
-bookRouter.post('/', 
-  asyncHandler(async (req, res) => {
-		const newBook = req.body;  // 포스트맨 바디가 여기 담겨온다
-		console.log(newBook);
-		if (!newBook.name || !newBook.start_lang || !newBook.end_lang || !newBook.ownerEmail) {
-			return next(new Error('Missing required fields')); 
-		}
-		const result = await bookService.createOne(newBook);
-		res.status(201).json(result);
-	})
-);
+bookRouter.post('/me', verifyToken, asyncHandler(async (req, res, next) => {
+	const newBook = req.body;  
+	newBook.ownerEmail = req.user.email;  
+	if (!newBook.name || !newBook.start_lang || !newBook.end_lang || !newBook.ownerEmail) {
+		return next(new Error('Missing required fields')); 
+	}
+	const result = await bookService.createOne(newBook);
+	res.status(201).json(result);
+}));
 
-bookRouter.delete('/:id', 
-	asyncHandler(async (req, res) => {
-		const { id } = req.params;
-		const result = await bookService.deleteOne({ short_id: id });
-		if (!result) {
-			return next(new Error('Book not found'));  
-		}
-		res.status(200).json({ message: "단어장이 삭제되었습니다" });
-	})
-);
+bookRouter.delete('/me/:id', verifyToken, asyncHandler(async (req, res, next) => {
+	const result = await bookService.deleteOne({ ownerEmail: req.user.email, short_id: req.params.id });
+	if (!result) {
+		return next(new Error('No book found for this user with the provided id'));  
+	}
+	res.status(200).json({ message: "단어장이 삭제되었습니다" });
+}));
 
-bookRouter.put('/short-id/:id', 
-	asyncHandler(async (req, res) => {
-		const { id } = req.params;
-		const updatedBook = req.body;
-		if (!updatedBook.name || !updatedBook.start_lang || !updatedBook.end_lang || !updatedBook.ownerEmail) {
-			return next(new Error('Missing required fields'));  
-		}
-		const result = await bookService.updateOne({ short_id: id }, updatedBook);
-		if (!result) {
-			return next(new Error('Book not found')); 
-		}
-		res.status(200).json(result);
-	})
-);
+bookRouter.put('/me/:id', verifyToken, asyncHandler(async (req, res, next) => {
+	const updatedBook = req.body;
+	const result = await bookService.updateOne({ ownerEmail: req.user.email, short_id: req.params.id }, updatedBook);
+	if (!result) {
+		return next(new Error('No book found for this user with the provided id')); 
+	}
+	res.status(200).json(result);
+}));
 
 module.exports = { bookRouter };
+
