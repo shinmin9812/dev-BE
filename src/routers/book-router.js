3f3bf0a38eaa1117a710c429b7cd624b9b829247
yubin <shin.yubin18@gmail.com>
2023-05-12 20:12:55 +0900

    Fix : book, word 스키마 수정중

--- src/routers/book-router.js
+++ src/routers/book-router.js
@@ -1,4 +1,11 @@
 const { Router } = require('express');
+
+const {
+	WordModel,
+	BookModel,
+	BookCaseModel,
+} = require('../db/schemas/word-schema');
+
 const { bookService } = require('../services/book-service');
 const { asyncHandler } = require('../middlewares/async-handler');
 const verifyToken = require('../middlewares/auth-handler');
@@ -13,16 +20,16 @@ bookRouter.get('/', verifyToken, asyncHandler(async (req, res) => {
 bookRouter.get('/me', verifyToken, asyncHandler(async (req, res, next) => {
 	const result = await bookService.findAllByUser({ ownerEmail: req.user.email });
 	if (!result) {
-		return next(new Error('사용자의 단어장이 없습니다'));  
+		return next(new Error('사용자의 단어장이 없습니다'));
 	}
 	res.status(200).json(result);
 }));
 
 bookRouter.post('/me', verifyToken, asyncHandler(async (req, res, next) => {
-	const newBook = req.body;  
-	newBook.ownerEmail = req.user.email;  
+	const newBook = req.body;
+	newBook.ownerEmail = req.user.email;
 	if (!newBook.name || !newBook.start_lang || !newBook.end_lang || !newBook.ownerEmail) {
-		return next(new Error('Missing required fields')); 
+		return next(new Error('Missing required fields'));
 	}
 	const result = await bookService.createOne(newBook);
 	res.status(201).json(result);
@@ -31,7 +38,7 @@ bookRouter.post('/me', verifyToken, asyncHandler(async (req, res, next) => {
 bookRouter.delete('/me/:id', verifyToken, asyncHandler(async (req, res, next) => {
 	const result = await bookService.deleteOne({ ownerEmail: req.user.email, short_id: req.params.id });
 	if (!result) {
-		return next(new Error('No book found for this user with the provided id'));  
+		return next(new Error('No book found for this user with the provided id'));
 	}
 	res.status(200).json({ message: "단어장이 삭제되었습니다" });
 }));
@@ -40,7 +47,7 @@ bookRouter.put('/me/:id', verifyToken, asyncHandler(async (req, res, next) => {
 	const updatedBook = req.body;
 	const result = await bookService.updateOne({ ownerEmail: req.user.email, short_id: req.params.id }, updatedBook);
 	if (!result) {
-		return next(new Error('No book found for this user with the provided id')); 
+		return next(new Error('No book found for this user with the provided id'));
 	}
 	res.status(200).json(result);
 }));
