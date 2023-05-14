3d7273d339cc0ed27c34bf44ee0848108e7bf049
yubin <shin.yubin18@gmail.com>
2023-05-14 12:12:36 +0900

    Add : 검색기능, 관리자용 단어 게시 기능
    1.검색기능
    aggregate 불안정 확인, find 로 변경
    차후 검색기능 고도화 시 index 사용 권장드립니다.
    
    2.관리자용 단어 대량 게시 기능
    크롤링 기능과 결합해두었습니다. 관리자용 초기화에 유용합니다.

--- src/routers/search-router.js
+++ src/routers/search-router.js
@@ -1,47 +1,18 @@
 const { Router } = require('express');
-
 const { WordModel } = require('../db/schemas/word-schema');
 const { BookModel } = require('../db/schemas/book-schema');
 const { wordService } = require('../services/word-service');
 const { asyncHandler } = require('../middlewares/async-handler');
 const verifyToken = require('../middlewares/auth-handler');
-const { wordMeaningService } = require('../services/wordMeaning-service');
 const searchRouter = Router();
 
 searchRouter.get(
 	'/words',
 	verifyToken,
 	asyncHandler(async (req, res) => {
-		// await WordModel
 		const { userEmail } = req.user;
-		console.log(req.query.word);
-		const searchCondition = [
-			{
-				$search: {
-					text: {
-						query: req.query.word,
-						path: 'word'
-					}
-				}
-			},
-			{
-				$match: {
-					ownerEmail: userEmail
-				}
-			},
-			{
-				$sort: {
-					createdAt: -1
-				}
-			}
-		];
-		const result1 = await WordModel.aggregate(searchCondition);
-		console.log('result1:', result1);
-
-		// const result2 = await WordModel.find({ ownerEmail: userEmail, word: req.query.word });
-		// console.log('result2:', result2);
-		res.status(200).json({ result1 });
-
+		const result = await WordModel.find({ ownerEmail: userEmail, word: req.query.word });
+		res.status(200).json(result);
 	})
 );
 
@@ -50,33 +21,8 @@ searchRouter.get(
 	verifyToken,
 	asyncHandler(async (req, res) => {
 		const { userEmail } = req.user;
-		console.log(req.query.name);
-		const searchCondition = [
-			{
-				$search: {
-					text: {
-						query: req.query.name,
-						path: "name"
-					}
-				}
-			},
-			{
-				$match: {
-					ownerEmail: userEmail
-				}
-			},
-			{
-				$sort: {
-					createdAt: -1
-				}
-			}
-		];
-		const result1 = await BookModel.aggregate(searchCondition);
-		console.log('result1:', result1);
-		const result2 = await BookModel.find({ ownerEmail: userEmail, name: req.query.name });
-		console.log('result2:', result2);
-		res.status(200).json(result2);
-
+		const result = await BookModel.find({ ownerEmail: userEmail, name: req.query.name });
+		res.status(200).json(result);
 	})
 );
 
--- src/routers/word-router.js
+++ src/routers/word-router.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const { wordService } = require('../services/word-service');
 const { asyncHandler } = require('../middlewares/async-handler');
 const verifyToken = require('../middlewares/auth-handler');
-
+const { meaningService } = require('../services/meaning-service');
 const wordRouter = Router();
 
 wordRouter.get(
@@ -59,6 +59,7 @@ wordRouter.post(
 		}
 	}),
 );
+
 wordRouter.delete(
 	'/:id',
 	verifyToken,
@@ -84,5 +85,65 @@ wordRouter.put(
 		res.status(200).json(result);
 	}),
 );
+
+
+/**관리자가 한번에 샘플 단어장 생성시 사용 */
+wordRouter.post(
+	'/admin',
+	verifyToken,
+	asyncHandler(async (req, res) => {
+		const { userEmail } = req.user;
+		/**배열로 여러개 생성하려한다면 */
+		if (Array.isArray(req.body)) {
+			const newWordsArray = req.body;
+			const promises = newWordsArray.map(async (word, index) => {
+				return new Promise((resolve, reject) => {
+					setTimeout(async () => {
+						try {
+							const meanings = await meaningService.getWordMeanings(
+								word.lang,
+								word.word,
+							);
+							word.ownerEmail = userEmail;
+							word.meanings = meanings;
+							console.log(word);
+							resolve(word);
+						} catch (err) {
+							console.log(
+								`Error occurred while fetching the meanings for ${word.word}: ${err.message}`,
+							);
+							resolve(null);
+						}
+					}, index * 500); // 각 요청 사이의 지연 시간을 인덱스 값에 따라 설정
+				});
+			});
+			Promise.all(promises)
+				.then(results => {
+					const filteredWords = results.filter(word => word !== null);
+					return wordService.createMany(filteredWords);
+				})
+				.then(result => {
+					res.status(200).json(result);
+				})
+				.catch(err => {
+					console.log(`Error occurred: ${err.message}`);
+					res.status(500).json({ message: 'Internal server error' });
+				});
+		} else {
+			/**하나만 생성하려한다면 */
+			const { lang, word } = req.body;
+			const meanings = await meaningService.getWordMeanings(lang, word);
+			const newWord = req.body;
+			newWord.ownerEmail = userEmail;
+			newWord.meanings = meanings;
+			const result = await wordService.createOne(newWord);
+			console.log(result);
+			res.status(200).json(result);
+		}
+	}),
+);
+
+
+
 
 module.exports = { wordRouter };
