634c78617e8e832fd2bc5af735cff0747424f7ea
shinmin <shinmin9812@naver.com>
2023-05-19 22:07:30 +0900

    Feat: 사지선다 문제 만들기 구현

--- src/routers/quiz-router.js
+++ src/routers/quiz-router.js
@@ -1,7 +1,31 @@
 const { Router } = require('express');
 const verifyToken = require('../middlewares/auth-handler');
+const { asyncHandler } = require('../middlewares/async-handler');
 const QuizModel = require('../db/schemas/quiz-schema');
+const { wordService } = require('../services/word-service');
 const quizRouter = Router();
+
+//민석 시작===========================================
+quizRouter.get(
+	'/four-prong',
+	verifyToken,
+	asyncHandler(async (req, res) => {
+		const { userEmail } = req.user;
+		const bookIds = req.query.bookId.split(',');
+		const number = parseInt(req.query.number);
+		const statuses = req.query.status.split(',').map(Number);
+
+		const response = await wordService.findWordsByBookAndStatus(
+			userEmail,
+			statuses,
+			bookIds,
+			number
+		);
+
+		res.status(200).json(response);
+	}),
+);
+//민석 끝===========================================
 
 quizRouter.post(
 	'/',
